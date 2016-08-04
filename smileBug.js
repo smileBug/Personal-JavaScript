f4a47818a89332591ce3508237c5d3de09ffe20c
smilebug <yzh8275283@gmail.com>
2016-08-04 19:55:35 +0800

    Eventutil

--- smileBug.js
+++ smileBug.js
@@ -21,3 +21,57 @@ function insertAfter(newElement,targetElement){
 		parent.insertBefore(newElement,targetElement.nextSibling);
 	}
 }
+
+//事件
+var EventUtil = {
+
+	//添加事件处理程序
+	addHandler : function(element,type,handler){
+		if (element.addEventListener) {
+			element.addEventListener(type,handler,false);
+		}else if (element.attachEvent) {
+			element.attachEvent('on' + type,handler);
+		}else {
+			element['on' + type] = handler;
+		}
+	},
+
+	//返回对event对象的引用
+	getEvent : function(event){
+		return event ? event : window.event;
+	},
+
+	//返回事件的目标
+	getTarget : function(event){
+		return event.target || event.srcElement;
+	},
+
+	//取消事件的默认行为
+	preventDefault : function(event){
+		if (event.preventDefault) {
+			event.preventDefault();
+		}else {
+			event.returnValue = false;
+		}
+	},
+
+	//删除事件处理程序
+	removeHandler : function(element,type,handler){
+		if (element.removeEventListener) {
+			element.removeEventListener(type,handler,false);
+		}else if (element.detachEvent) {
+			element.detachEvent('on' + type,handler);
+		}else {
+			element['on' + type] = null;
+		}
+	},
+
+	//阻止事件流
+	stopPropagation : function(event){
+		if (event.stopPropagation) {
+			event.stopPropagation();
+		}else {
+			event.cancebubble = true;
+		}
+	}
+};
